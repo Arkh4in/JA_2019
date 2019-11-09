a79bdba2d7b229807df4edcd329f45662674c45d
Melissa Sadouki <msadouki@cremi.emi.u-bordeaux1.fr>
2019-11-09 14:51:12 +0100

    Add Base classes

--- JA_19/JA_19/Program.cs
+++ JA_19/JA_19/Program.cs
@@ -8,8 +8,15 @@ namespace JA_19
 {
     class Program
     {
+        //Prop & Constructors//
         static void Main(string[] args)
         {
+            Layout background = new Layout(32,32);
+            background.Display();
+
+            Vector2 roomCoordinates = new Vector2(5, 3);
+
+            Room room = new Room(3, roomCoordinates, 3, 8);
         }
     }
 }
---
+++ JA_19/JA_19/Vector2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JA_19
+{
+    public class Vector2
+    {
+        public int x { get; private set; }
+        public int y { get; private set; }
+
+        public Vector2(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+}
